0cef82ed50a1a8da9df853e0329e909b13c83100
Robbie Jackson <robbie.jackson@btinternet.com>
2026-07-03 16:26:51 +0100

    fix accessibility issues

--- component-tutorial/step06_admin_list/com_example/administrator/components/com_example/tmpl/landmarks/default.php
+++ component-tutorial/step06_admin_list/com_example/administrator/components/com_example/tmpl/landmarks/default.php
@@ -6,21 +6,24 @@ use Joomla\CMS\Language\Text;
 
 ?>
 <table class="table">
+    <caption class="visually-hidden">
+        <?php echo Text::_('COM_EXAMPLE_LANDMARKS_CAPTION'); ?>
+    </caption>
     <thead>
         <tr>
-            <th>
+            <th scope="col">
                 <?php echo Text::_('JGLOBAL_TITLE'); ?>
             </th>
-            <th>
+            <th scope="col">
                 <?php echo Text::_('JGRID_HEADING_ID'); ?>
             </th>
         </tr>
     </thead>
     <tbody><?php foreach ($this->items as $i => $item) :?>
                 <tr>
-                    <td>
+                    <th scope="row">
                         <?php echo $item->title; ?>
-                    </td>
+                    </th>
                     <td>
                         <?php echo $item->id; ?>
                     </td>
